eb040d7ebeae91f3781fd33cb41cc2cb9ef341e2
Mina C <mina.choe@outlook.com>
2019-07-27 01:48:21 +0900

    Add sql alchemy library and database

--- webserver.py
+++ webserver.py
@@ -1,5 +1,13 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi                                                       # decipher the message that was sent from the server
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
 
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
