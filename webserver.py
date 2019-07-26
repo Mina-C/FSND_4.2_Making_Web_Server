acc9d569428faa0d1d7808c30f3ebf5ae4433aa4
Mina C <mina.choe@outlook.com>
2019-07-27 01:56:38 +0900

    Fix wrong indentation and reposition 'try'

--- webserver.py
+++ webserver.py
@@ -10,28 +10,27 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 class webserverHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def do_GET(self):        
         try:
             if self.path.endswith("/restaurants"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+    
+                restaurants = session.query(Restaurant).all()
+                output = ""
+                output += "<html><body>"
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "<a href='#'>Edit</a>"
+                    output += "</br>"
+                    output += "<a href='#'>Delete</a>"
+                    output += "</br></br>"
+                output += "</html></body>"
+                self.wfile.write(output)
+                print output
+                return
 
-            restaurants = session.query(Restaurant).all()
-            output = ""
-            output += "<html><body>"
-            for restaurant in restaurants:
-                output += restaurant.name
-                output += "<a href='#'>Edit</a>"
-                output += "</br>"
-                output += "<a href='#'>Delete</a>"
-                output += "</br></br>"
-            output += "</html></body>"
-            self.wfile.write(output)
-            print output
-            return
-        
-        try:
             if self.path.endswith("/hello"):                     # find URL ends with '/hello'
                 self.send_response(200)                          # if yes, send 200; successful GET request
                 self.send_header('Content-type', 'text/html')    # telling to client that we will reply text in the form of HTML
