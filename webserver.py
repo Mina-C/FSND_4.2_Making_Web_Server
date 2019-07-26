bf472d32c61bc7ca401184610da4b56e9cda986b
Mina C <mina.choe@outlook.com>
2019-07-27 01:37:07 +0900

    Add webserverHandler() to send "Hello" to client

--- webserver.py
+++ webserver.py
@@ -1,5 +1,22 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
+class webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):                     # find URL ends with '/hello'
+                self.send_response(200)                          # if yes, send 200; successful GET request
+                self.send_header('Content-type', 'text/html')    # telling to client that we will reply text in the form of HTML
+                self.end_headers()                               # sends blank line, end of HTTP headers in the response
+
+                output = ""                                      # empty for now
+                output += "<html><body>Hello!</body></html>"
+                self.wfile.write(output)                         # sending a message back to the client
+                print output                                     # print for myself
+                return                                           # to exit if statement
+
+        except IOErrors:
+            self.send_error(404, "File Not Found %s" % self.path)    # to notify me error
+
 def main():
     try:
         port = 8080 # port number is integer
