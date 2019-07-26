a26590a144feb16a44817b9039ebcfc29afb5a7f
Mina C <mina.choe@outlook.com>
2019-07-27 01:46:39 +0900

    Fix error ; a enter middle of the command

--- webserver.py
+++ webserver.py
@@ -48,8 +48,7 @@ class webserverHandler(BaseHTTPRequestHandler):
             output += "<h2>OK, how about this: </h2>"
             output += "<h1> %s </h1>" % messagecontent[0]
 
-            output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2>
-            <input name='message' type='text'><input type='submit' value='Submit'> </form>"
+            output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'> </form>"
             output += "</body></html>"
             self.wfile.write(output)
             print output
